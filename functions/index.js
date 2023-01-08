d1a1aee663c59cdcd8b9380b57673be78f99f987
Wendesen22 <116609384+Wendesen22@users.noreply.github.com>
2023-01-08 14:49:38 -0600

    Components and server side added

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+"sk_test_51MMRdbHugCruEOaiRkKCWnLGjn727bb3HNEWRf4IjIanUf9cCyKZD2X7xVeZ6GkaNE6DQ2T9wfJfj0IAd5vTzXMg001gJ3Ca5A");
+// - App config
+const app = express();
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
---
+++ src/axios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const instance = axios.create({
+	baseURL: "https://us-central1-replica-3c221.cloudfunctions.net/api",
+});
+export default instance;
+// http://127.0.0.1:5001/replica-3c221/us-central1/api
+
